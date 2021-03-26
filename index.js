c87c6c9ccae0a496b076f7c7d17866adaa5acff7
Ira Maier <73794984+maipy@users.noreply.github.com>
2021-03-26 09:01:49 +0100

    Delete index.js
    for search

--- index.js
+++
@@ -1,8 +0,0 @@
-const algoliasearch = require('algoliasearch');
-
-const client = algoliasearch('YM765JKKDH', 'd8d253ff98490bb748b554ae121526c6');
-const index = client.initIndex('chemotion_newsroom_docs');
-
-index.exists().then(result => {
-  console.log(result);
-});
