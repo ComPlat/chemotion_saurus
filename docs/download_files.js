a6b162f22c2d72318763c804fcd67f9df4820194
maipy <ira.maier@kit.edu>
2021-05-15 16:27:32 +0200

    updated download files

--- docs/download_files.js
+++ docs/download_files.js
@@ -1,42 +1,21 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faDownload} from '@fortawesome/free-solid-svg-icons'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
 
-
-function download(files){
-    // TODO 
-    // return (
-    //     <a target="_blank" href="/chemotionsaurus/files/template%20_sample_import.xlsx" download="template_sample_import.xlsx"></a>);
-
-    setTimeout(() => {
+function download(files) {
     files.forEach((f) => {
-        // alert(f);
-        // console.log(f);
-        
-    //     return (
-    //     <a target="_blank" href={f} download={f.split('/')[-1]}></a>);
-    //     debugger;
-    //    // <a target="_blank" href="/chemotionsaurus/files/template%20_sample_import.xlsx" download="template_sample_import.xlsx">Download href</a>
+        const filename = f.split('/').pop()
+        const a = document.createElement("a");
+        a.download = filename;
+        a.href = f;
+        a.click();
+        a.remove();
     });
-}, 5000);
-}
-
-function getBtn(props){
-return(
-<button class="button button--primary" name="downloadBtn" onClick={download(props.files)}>{props.text}<FontAwesomeIcon icon={faDownload}/></button> );
 }
 
 export function DownloadBtn (props){
-
-    // return{
-    //     async getBtn(props){
-    //         await downloadBtn(props.files);
-    //     }
-    // }
-
-    return(
-    <button className="button button--primary" name="downloadBtn" onClick={download(props.files)}>{props.text}<FontAwesomeIcon icon={faDownload}/></button> );
-
+        return(
+            // https://reactjs.org/docs/faq-functions.html
+            <button className="button button--primary" name="downloadBtn" onClick={() => download(props.files)}>{props.text}<FontAwesomeIcon icon={faDownload}/></button> 
+            );
 };
-
-// export default Download;
