076e0f9821e85ea696fae4816d11c0839e3f9250
maipy <ira.maier@kit.edu>
2021-06-18 20:14:24 +0200

    revert baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ module.exports = {
     'Electronic Laboratory Notebook (ELN) &\n' +
     'Repository for Research Data\n',
   url: 'https://chemotion.net',
-  baseUrl: '/',
-  // baseUrl: '/chemotionsaurus/',
+  //baseUrl: '/',
+  baseUrl: '/chemotionsaurus/',
   onBrokenLinks: 'log',
   // onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
