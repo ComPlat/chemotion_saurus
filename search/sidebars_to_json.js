eb02b4906731f57e39a6bd3fd03f8d4ab986b473
Danny Truong <allenskywalker92@gmail.com>
2022-09-12 11:20:01 +0200

    using theme classic instead of custom

--- search/sidebars_to_json.js
+++
@@ -1,9 +0,0 @@
-fs = require('fs');
-
-let sidebarJS = require('../sidebars.js')
-
-sidebarJSON = JSON.stringify(sidebarJS)
-
-fs.writeFile('sidebars.json', sidebarJSON, function (err) {
-    if (err) return console.log(err);
-  });
